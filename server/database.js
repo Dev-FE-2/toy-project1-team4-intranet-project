5c1933744953c4dc2eecec4a75bf7fce576bc041
whwjdan <whwjdan@naver.com>
2024-10-18 14:06:13 +0900

    fix: eslint 설정 변경으로 인한 파일  변경

--- server/database.js
+++ server/database.js
@@ -1,10 +1,10 @@
-import sqlite3 from "sqlite3";
+import sqlite3 from 'sqlite3';
 
-const databaseName = "toyprj1";
+const databaseName = 'toyprj1';
 const database = new sqlite3.Database(`./${databaseName}.db`);
 
 database.serialize(() => {
-  database.run(`
+	database.run(`
     CREATE TABLE IF NOT EXISTS Users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       userId TEXT NOT NULL,
