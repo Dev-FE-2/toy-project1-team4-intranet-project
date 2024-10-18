081f4a78f19df8236e158857b439db151e4bca97
whwjdan <whwjdan@naver.com>
2024-10-18 14:04:06 +0900

    fix: prettier-trailingComma-option-change-to-all

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  rules: {
-    semi: ["error", "always"],
-    quotes: ["error", "double"],
-    "no-unused-vars": "off",
-  },
+	env: {
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+	rules: {
+		'no-unused-vars': 'off',
+	},
 };
