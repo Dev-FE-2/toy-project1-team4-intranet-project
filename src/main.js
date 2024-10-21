87e8a0841cf912319e6c02348e15e9b1a1a4a346
gold <goldegg127@gmail.com>
2024-10-21 23:57:29 +0900

    design: pc 화면 레이아웃 추가

--- src/main.js
+++ src/main.js
@@ -1,25 +1,45 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
+import './global.css';
+import Logo from '/public/logo.svg';
 import { setupCounter } from './counter.js';
 
 async function app() {
 	document.querySelector('#app').innerHTML = `
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="${viteLogo}" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-      </a>
-      <h1>Okay</h1>
-      <div class="card">
-        <button id="counter" type="button"></button>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite logo to learn more
-      </p>
-    </div>
+    <div class="layout">
+			<div class="layout__left">
+				<header class="gnb">
+					<a href="/"><img class="logo" src="${Logo}" alt="EVEN" /></a>
+					<nav class="nav">
+						<ul>
+							<li>
+								<a href="/" class="nav-item active"><i class="icon--home"></i> 마이페이지</a>
+							</li>
+							<li>
+								<a href="/profile/:user" class="nav-item"><i class="icon--profile"></i> 프로필</a>
+							</li>
+							<li>
+								<a href="/vacation" class="nav-item"><i class="icon--timetable"></i> 근태 신청</a>
+							</li>
+							<li>
+								<a href="/notice" class="nav-item"><i class="icon--notice"></i> 공지사항</a>
+							</li>
+							<li>
+								<a href="/admin/employees" class="nav-item"
+									><i class="icon--employee"></i> 직원 구성원</a
+								>
+							</li>
+						</ul>
+					</nav>
+				</header>
+			</div>
+			<div class="layout__right">
+				<aside class="top-bar">
+					<div class="avatar"></div>
+				</aside>
+				<main class="page-container">
+					<div id="contents" class="contents"></div>
+				</main>
+			</div>
+		</div>
   `;
 
 	setupCounter(document.querySelector('#counter'));
