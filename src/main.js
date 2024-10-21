7dd6e99d93d2983ad256bf16bca89da43026c812
gold <goldegg127@gmail.com>
2024-10-22 00:00:19 +0900

    design: sample 컨텐츠 마크업 추가

--- src/main.js
+++ src/main.js
@@ -36,7 +36,27 @@ async function app() {
 					<div class="avatar"></div>
 				</aside>
 				<main class="page-container">
-					<div id="contents" class="contents"></div>
+					<div id="contents" class="contents">
+						<section>
+							<h1 class="page-title">페이지 타이틀</h1>
+
+							<form action="" class="form">
+								<fieldset>
+									<legend>이름</legend>
+									<input type="text" class="form-item" placeholder="이름" />
+								</fieldset>
+								<fieldset>
+									<legend>포지션</legend>
+									<input type="text" class="form-item" placeholder="포지션" />
+								</fieldset>
+								<div class="btn-wrap">
+									<button class="btn btn--secondary" type="button">취소</button>
+									<button class="btn btn--primary" type="submit">확인</button>
+								</div>
+                <div id="counter"></div>
+							</form>
+						</section>
+					</div>
 				</main>
 			</div>
 		</div>
