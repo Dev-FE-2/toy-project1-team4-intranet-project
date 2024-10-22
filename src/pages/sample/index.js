e3e99073f09b1ae6ae86d01f7bd83c464c2bdaa9
goldegg127 <goldegg127@gmail.com>
2024-10-22 12:21:21 +0900

    [✨ Feature] 프로젝트 페이지 레이아웃 생성과 스타일 정의 (#16)
    * design: 노토산스 폰트와 로고이미지 적용
    
    * design: pc 화면 레이아웃 추가
    
    * design: sample 컨텐츠 마크업 추가
    
    * refactor: layout UI와 샘플페이지 UI 모듈화

---
+++ src/pages/sample/index.js
@@ -0,0 +1,22 @@
+export default function SamplePage() {
+	return `<section>
+                <h1 class="page-title">페이지 타이틀</h1>
+
+                <form action="" class="form">
+                    <fieldset>
+                        <legend>이름</legend>
+                        <input type="text" class="form-item" placeholder="이름" />
+                    </fieldset>
+                    <fieldset>
+                        <legend>포지션</legend>
+                        <input type="text" class="form-item" placeholder="포지션" />
+                    </fieldset>
+                    <div class="btn-wrap">
+                        <button class="btn btn--secondary" type="button">취소</button>
+                        <button class="btn btn--primary" type="submit">확인</button>
+                    </div>
+                </form>
+
+                <div id="counter"></div>
+            </section>`;
+}
