0e15db3288415fb45944ef9a4f6936a011b25134
gold <goldegg127@gmail.com>
2024-10-28 16:06:17 +0900

    fix: url 경로를 통한 route 동작 이슈 수정

--- src/main.js
+++ src/main.js
@@ -3,22 +3,24 @@ import Layout from './components/Layout';
 import { route } from './router/route';
 
 async function app() {
-	const layout = new Layout();
-	document.querySelector('#app').innerHTML = layout.render();
 	init();
+	route();
 }
 
 const init = () => {
-	const navElements = document.querySelector('.nav');
+	const layout = new Layout();
+	document.querySelector('#app').innerHTML = layout.render();
 
 	window.addEventListener('popstate', route);
+
+	const navElements = document.querySelector('.nav');
 	navElements.addEventListener('click', navigate);
 };
 
 const navigate = (event) => {
 	event.preventDefault();
 
-	// const path = event.target.getAttribute('href'); // a 요소 하위에 다른 요소를 둘러싸면 클릭버블링이 안생긴다.
+	// const path = event.target.getAttribute('href'); // a 요소 하위에 다른 요소를 둘러싸면 클릭 이벤트 버블링이 안생긴다.
 
 	const anchor = event.target.closest('a');
 
