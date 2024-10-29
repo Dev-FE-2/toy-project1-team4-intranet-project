da67958518bfde6270db4d7f174d6d9bd1a2f6ca
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-10-29 14:31:00 +0900

    feat:employeeAddPage imageUpload (#36)

--- src/pages/EmployeeAdd/index.js
+++ src/pages/EmployeeAdd/index.js
@@ -1,14 +1,8 @@
 import './style.css';
-import { ProfileImage } from '../../components/pages/Profile/ProfileImage';
 import { ProfileForm } from '../../components/pages/Profile/ProfileForm';
 
 class EmployeeAdd {
 	constructor() {
-		this.buttons = `
-			<div class="btn-wrap">
-				<button class="btn btn--primary" type="submit">등록하기</button>
-			</div>
-		`;
 		this.emptyUserData = {
 			name: '',
 			job: '',
@@ -18,23 +12,95 @@ class EmployeeAdd {
 			bio: '',
 			profileImage: '../../../public/avatar.svg',
 		};
+
+		this.profileImageSrc = this.emptyUserData.profileImage;
+		this.isImageUploaded = false;
+
+		this.profileImageInput = document.createElement('input');
+		this.profileImageInput.type = 'file';
+		this.profileImageInput.accept = 'image/*';
+		this.profileImageInput.style.display = 'none';
+		this.profileImageInput.addEventListener('change', this.handleImageUpload.bind(this));
+	}
+
+	handleImageUpload(event) {
+		const file = event.target.files[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = (e) => {
+				this.profileImageSrc = e.target.result;
+				this.isImageUploaded = true;
+				this.fileName = file.name;
+				this.updateProfileImage();
+			};
+			reader.readAsDataURL(file);
+		}
+	}
+
+	handleImageRemove() {
+		this.profileImageSrc = this.emptyUserData.profileImage;
+		this.isImageUploaded = false;
+		this.fileName = '';
+		this.updateProfileImage();
+	}
+
+	updateProfileImage() {
+		const imageElement = document.querySelector('.profile-image img');
+		const fileNameElement = document.querySelector('.file-name');
+		const deleteButton = document.querySelector('.delete-btn');
+		if (imageElement) {
+			imageElement.src = this.profileImageSrc;
+		}
+		if (fileNameElement) {
+			fileNameElement.textContent = this.isImageUploaded ? this.fileName : '';
+		}
+		if (deleteButton) {
+			deleteButton.style.display = this.isImageUploaded ? 'inline-block' : 'none';
+		}
 	}
 
 	render() {
+		const fileNameSection = `
+			<div class="file-info">
+				<span class="file-name"></span>
+				<button class="delete-btn" onclick="window.employeeAddInstance.handleImageRemove()" style="display: none;">x</button>
+			</div>
+		`;
+
+		this.buttons = `
+			<div class="btn-wrap">
+				<button class="btn btn--primary" type="submit">등록하기</button>
+			</div>
+		`;
+
+		const profileImageSection = `
+			<div class="profile-image-wrapper">
+				<div class="profile-image" onclick="window.employeeAddInstance.profileImageInput.click()">
+					<img src="${this.profileImageSrc}" alt="Profile Image" />
+				</div>
+				${fileNameSection}
+			</div>
+		`;
+
+		document.body.appendChild(this.profileImageInput);
+
 		return `
-		<section class="contents">
-			<div class="container">
-				<h1 class="page-title">직원 등록</h1>
-				<div class="profile-container">
-					<div class="profile">
-						${ProfileImage(this.emptyUserData.profileImage)}
-						${ProfileForm(this.emptyUserData, this.buttons)}
+			<section class="contents">
+				<div class="container">
+					<h1 class="page-title">직원 등록</h1>
+					<div class="profile-container">
+						<div class="profile">
+							${profileImageSection}
+							${ProfileForm(this.emptyUserData, this.buttons)}
+						</div>
 					</div>
 				</div>
-			</div>
 			</section>
 		`;
 	}
 }
+
+window.employeeAddInstance = new EmployeeAdd();
+document.querySelector('#app').innerHTML = window.employeeAddInstance.render();
 
 export default EmployeeAdd;
