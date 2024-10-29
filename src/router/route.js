c0ba9ae1380be5d067129e3f2c679ee9cc16b0e6
gold <goldegg127@gmail.com>
2024-10-29 23:05:00 +0900

    chore: 사용하지 않는 코드와 파일 삭제

--- src/router/route.js
+++ src/router/route.js
@@ -14,18 +14,13 @@ import {
 	EmployeeAddPage,
 } from '../pages';
 
-export const pushContents = (page) => {
-	const contentsElements = document.querySelectorAll('#pageContents');
-	contentsElements.forEach((element) => {
-		element.innerHTML = page;
-	});
-};
-
 export const route = () => {
 	const path = window.location.pathname;
 
 	const navigation = new Navigation();
 	navigation.updateActiveMenu();
+
+	const contentsElement = document.querySelector('#pageContents');
 
 	switch (true) {
 		case path === url.home:
@@ -36,56 +31,56 @@ export const route = () => {
 			const userId = path.match(urlPattern.userProfile)[1];
 			console.log('userId', userId);
 
-			pushContents(new ProfilePage(userId).render());
+			contentsElement.innerHTML = new ProfilePage(userId).render();
 			break;
 		}
 
 		case path === url.vacation:
-			pushContents(new VacationPage().render());
+			contentsElement.innerHTML = new VacationPage().render();
 			break;
 
 		case path === url.notice:
-			pushContents(new NoticePage().render());
+			contentsElement.innerHTML = new NoticePage().render();
 			break;
 
 		case urlPattern.noticeDetail.test(path): {
 			const noticeId = path.match(urlPattern.noticeDetail)[1];
 			console.log('noticeId', noticeId);
 
-			pushContents(new NoticeDetailPage(noticeId).render());
+			contentsElement.innerHTML = new NoticeDetailPage(noticeId).render();
 			break;
 		}
 
 		case path === url.employeeList:
-			pushContents(new EmployeeListPage().render());
+			contentsElement.innerHTML = new EmployeeListPage().render();
 			break;
 
 		case path === url.employeeAdd:
-			pushContents(new EmployeeAddPage().render());
+			contentsElement.innerHTML = new EmployeeAddPage().render();
 			break;
 
 		case urlPattern.employeeDetail.test(path): {
 			const employeeId = path.match(urlPattern.employeeDetail)[1];
 			console.log('employee userId', employeeId);
 
-			pushContents(new EmployeeDetailPage(employeeId).render());
+			contentsElement.innerHTML = new EmployeeDetailPage(employeeId).render();
 			break;
 		}
 
 		case path === url.login:
-			pushContents(new LoginPage().render());
+			contentsElement.innerHTML = new LoginPage().render();
 			break;
 
 		case urlPattern.sample.test(path): {
 			const id = path.match(urlPattern.sample)[1];
 			console.log('saple id', id);
 
-			pushContents(new SamplePage(id).render());
+			contentsElement.innerHTML = new SamplePage(id).render();
 			break;
 		}
 
 		default:
-			pushContents(new NotFound404Page().render());
+			contentsElement.innerHTML = new NotFound404Page().render();
 			break;
 	}
 };
