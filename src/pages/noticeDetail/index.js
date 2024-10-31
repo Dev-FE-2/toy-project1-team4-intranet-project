3f661c9e8874b89e6a33a3001dd45e1fcb1bd112
Mojo <iamsomojo@gmail.com>
2024-10-31 14:16:28 +0900

    [✨ Feature] 공지사항 페이지 (#55)
    * feat: 공지사항 페이지 Init (#54)
    
    * feat: 공지사항 페이지 구현 (#54)
    
    * feat: 공지사항 상세 페이지 구현 (#54)
    
    * feat: route.js 파일 공지사항 관련 변경 (#54)

--- src/pages/noticeDetail/index.js
+++ src/pages/noticeDetail/index.js
@@ -1,18 +1,43 @@
 import './style.css';
+import noticeData from '../notice/noticeList';
 
-export default function NoticeDetail() {
-	return `<section class="notice-detail">
-		<h1 class="notice-title">공지사항 제목입니다</h1>
+export default class NoticeDetail {
+	constructor(noticeId) {
+		this.noticeId = noticeId;
+	}
+	async fetchNoticeDetailData(url) {
+		try {
+			const res = await fetch(url);
+			if (!res.ok) {
+				throw new Error(`${res.statusText}`);
+			}
+			return await res.json();
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+	}
+
+	getNoticeDetailData(data, findNumber) {
+		return data.findIndex((item) => item.notice_id === findNumber);
+	}
+
+	render() {
+		const notice = noticeData[this.getNoticeDetailData(noticeData, +this.noticeId)];
+
+		return `<section class="notice-detail">
+		<h1 class="notice-title">${notice.title}</h1>
 		<div class="notice-info">
-			<span class="notice-info__writer">김철수</span>
+			<span class="notice-info__writer">${notice.created_date}</span>
 			<span class="separator">|</span>
-			<span class="notice-info__cdate">2024.10.10</span>
+			<span class="notice-info__cdate">${notice.writer}</span>
 		</div>
 		<div class="notice-detail__container">
-			<img class="notice-detail__img" src="https://picsum.photos/800/300" alt="공지사항 이미지">
+			<img class="notice-detail__img" src="${notice.image_url}" alt="공지사항 이미지">
 				<p class="notice-detail__article">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur aut commodi debitis id nisi odit officiis quaerat, quidem sequi. Architecto aspernatur enim iure quis voluptates voluptatibus! Consequatur cum esse obcaecati tempore. Asperiores beatae cumque doloribus exercitationem magni porro quo sed! Ad blanditiis cupiditate, eos exercitationem harum itaque officia quod.
+				${notice.content}
 				</p>
 		</div>
 	</section>`;
+	}
 }
