0da7bf733103bbf9bb3b4221aff038e70a91738f
whwjdan <35955189+whwjdan@users.noreply.github.com>
2024-10-31 10:54:17 +0900

    [✨ Feature] 마이페이지 프로필 정보 가져오기 (#57)
    * feat: 근무 상태 아이콘 active 속성 추가
    
    * feat: 유저 정보 가져오기 기능 추가 (#56)
    
    * feat: 프로필 근무 상태 변경 기능 추가
    
    * fix: 필요없는 콘솔 로그 삭제
    
    * fix: 사용하지 않는 변수 삭제
    
    * style: 유저 프로필 데이터 이름 변경 및 포지션 데이터 변경

---
+++ src/pages/mypage/profile/userProfile.js
@@ -0,0 +1,28 @@
+// profile.js
+import userProfile from './userProfile.json';
+
+// 특정 user_id에 맞는 유저 정보를 반환하는 함수
+function getUserProfile(userId) {
+	return userProfile.find((user) => user.user_id === userId);
+}
+
+// 유저 정보를 받아 DOM에 업데이트하는 함수
+export function renderUserProfile(userId) {
+	const user = getUserProfile(userId);
+
+	if (user) {
+		// HTML 요소 선택
+		//const profileIcon = document.querySelector('.profile__icon img');
+		const profileName = document.querySelector('.profile-name');
+		const profilePosition = document.querySelector('.profile-position');
+
+		// todo 데이터 추가 이후 주석 해제 및 변경
+		// 요소 업데이트
+		// profileIcon.src = `../../../public/${user['profile-image']}`;
+		// profileIcon.alt = `${user['profile-name']}의 프로필 이미지`;
+		profileName.textContent = user['profile-name'];
+		profilePosition.textContent = user['profile-position'];
+	} else {
+		console.error('해당 user_id의 유저 정보를 찾을 수 없습니다.');
+	}
+}
