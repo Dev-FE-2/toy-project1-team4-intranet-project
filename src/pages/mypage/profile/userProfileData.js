ff38adee38edf7945e202a702c994678f16c7d15
whwjdan <whwjdan@naver.com>
2024-10-31 17:24:59 +0900

    feat: 마이페이지 가져오는 프로필 데이터 정보 수정 및 근태 프리뷰 기능 추가 (#65)

---
+++ src/pages/mypage/profile/userProfileData.js
@@ -0,0 +1,32 @@
+const userProfileData = [
+	{
+		userId: 'g-dragon123',
+		username: '이브이',
+		job: '매장 관리자',
+		team: null,
+		image: 'image1.jpg',
+	},
+	{
+		userId: 'sad123456',
+		username: '김직원',
+		job: '품질 관리 담당',
+		team: null,
+		image: 'image2.jpg',
+	},
+	{
+		userId: 'fdsaf',
+		username: '이준석',
+		job: '매장식음료 관리자',
+		team: null,
+		image: 'image3.jpg',
+	},
+	{
+		userId: 'dddd',
+		username: '최은지',
+		job: '고객 서비스 담당',
+		team: null,
+		image: 'image4.jpg',
+	},
+];
+
+export default userProfileData;
