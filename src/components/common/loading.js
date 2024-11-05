751154287e10b463a14ae3e069c28f2e5f3755b2
gold <goldegg127@gmail.com>
2024-11-05 22:11:43 +0900

    feat: 로딩 UI 구현 (#105)

---
+++ src/components/common/loading.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+export default class Loading {
+	constructor(parentEl) {
+		this.parentEl = parentEl;
+		this.template = `<div class="spinner-container"><div class="spinner"></div></div>`;
+	}
+
+	remove() {
+		const loadingEl = this.parentEl.querySelector('.spinner-container');
+		console.log(loadingEl);
+		loadingEl.remove();
+	}
+
+	render() {
+		this.parentEl.innerHTML = this.template;
+	}
+}
