a5b98dafc82a8425335de28f21e1b944ef6d6d6f
gold <goldegg127@gmail.com>
2024-11-05 12:04:36 +0900

    rename: 대소문자만 바뀐 파일명이 git에 인식되지 않아서 다른 이름 적용 (#101)

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,7 +1,7 @@
-import IconHome from './IconHome';
-import IconProfile from './IconProfile';
-import IconTimetable from './IconTimetable';
-import IconNotice from './IconNotice';
-import IconEmployee from './IconEmployee';
+import IconHome from './iconHome2';
+import IconProfile from './iconProfile2';
+import IconTimetable from './iconTimetable2';
+import IconNotice from './iconNotice2';
+import IconEmployee from './iconEmployee2';
 
 export { IconHome, IconProfile, IconTimetable, IconNotice, IconEmployee };
