cf10a480dcad0dbd8e5ac237d954e3715878f0d3
gold <goldegg127@gmail.com>
2024-11-06 20:59:43 +0900

    feat: Express 서버 설정 기준으로 회원가입 api 함수 추가와 fetchUserData 함수 수정(#113)

--- src/apis/userProfileApi.js
+++ src/apis/userProfileApi.js
@@ -1,8 +1,44 @@
-async function fetchUserData() {
-	const response = await fetch('http://localhost:5173/api/user');
+async function postUserData({ username, job, team, phone, email, bio, profileImage }) {
+	const response = await fetch('http://localhost:3000/api/user', {
+		method: 'POST',
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			username,
+			job,
+			team,
+			phone,
+			email,
+			bio,
+			profile_image: profileImage,
+		}),
+	});
+
+	if (!response.ok) {
+		console.error(`postUserData Error: ${response}`);
+		throw new Error(`회원가입 요청에 실패했습니다. 다시 시도해 주세요.`);
+	}
+
 	const userData = await response.json();
+	console.log('userData', userData);
 
 	return userData;
 }
 
-export { fetchUserData };
+async function fetchUserData(userId) {
+	const response = await fetch(`http://localhost:3000/api/user/${userId}`);
+
+	if (!response.ok) {
+		console.error(`postUserData Error: ${response}`);
+		throw new Error(`사용자의 정보를 가져올 수 없습니다. 다시 시도해 주세요.`);
+	}
+
+	const userData = await response.json();
+	console.log('userData', userData);
+
+	return userData;
+}
+
+export { postUserData, fetchUserData };
