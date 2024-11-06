23664e3c5da72d1ba5846f40d18ddfc130811cd5
gold <goldegg127@gmail.com>
2024-11-07 02:47:02 +0900

    design: 서버 에러 UI 요소간 간격 추가 (#113)

--- src/components/common/error503/index.js
+++ src/components/common/error503/index.js
@@ -7,7 +7,7 @@ export default class Error503 {
 
 	constructor(parentEl) {
 		this.#parentEl = parentEl;
-		this.#template = `<div class="error" >
+		this.#template = `<div class="error">
 							<img src="${Img503}" alt="">
 							<p>일시적으로 중단되었습니다. <br>다시 시도해주세요.</p>
 						</div>`;
