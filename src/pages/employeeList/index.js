363a2f49b2f1038be1f6ed6ac7b8b8ccfb2a8a91
gold <goldegg127@gmail.com>
2024-11-08 11:58:38 +0900

    rename: 카멜케이스 컨벤션으로 폴더명, 파일명 이름 수정 (#122)

--- src/pages/employeeList/index.js
+++ src/pages/employeeList/index.js
@@ -1,8 +1,8 @@
 import './style.css';
 import employeesData from '../../../server/data/employees';
 import { url } from '../../router/url';
-import Pagination from '../../components/pages/employeeList/Pagination';
-import SearchBar from '../../components/pages/employeeList/Search';
+import Pagination from '../../components/pages/employeeList/pagination';
+import SearchBar from '../../components/pages/employeeList/search';
 
 const ITEMS_PER_PAGE = 10;
 
