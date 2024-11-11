4f30c61625cd61405a66672bdae807fd6636bc03
gold <goldegg127@gmail.com>
2024-11-11 17:57:30 +0900

    feat: 로그인, 회원가입 링크를 top bar에 노출 (#124)

--- src/components/layout/userStatus/index.js
+++ src/components/layout/userStatus/index.js
@@ -1,10 +1,47 @@
+import { url, urlLabel } from '../../../router';
 import './style.css';
 import Avatar from '/public/avatar.svg';
 
 export default class UserStatus {
-	render() {
+	#state;
+
+	constructor() {
+		this.#state = {
+			login: false,
+			path: window.location.pathname,
+		};
+	}
+
+	setState = (newState) => {
+		this.#state = { ...this.#state, ...newState };
+		this.render();
+	};
+
+	get #linkTemplate() {
+		const navList = ['login', 'signup'];
+
+		return `<nav class="login-links">
+			${navList
+				.map((page) => {
+					const className = this.#state.path === url[page] ? 'active' : '';
+
+					return `<a href="${url[page]}" class="${className}">${urlLabel[page]}</a>`;
+				})
+				.join('')}
+		</nav>`;
+	}
+
+	get #profile() {
+		return `<img class="avatar-img" src="${Avatar}" alt="프로필 사진" />`;
+	}
+
+	get #template() {
 		return `<div class="user-status">
-                <img class="avatar-img" src="${Avatar}" alt="프로필 사진" />
+                ${this.#state.login ? this.#profile : this.#linkTemplate}
             </div>`;
+	}
+
+	render() {
+		return this.#template;
 	}
 }
