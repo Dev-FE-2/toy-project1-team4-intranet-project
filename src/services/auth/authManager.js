309478bad285b257174007a2da456f0e2945b2a8
gold <goldegg127@gmail.com>
2024-11-12 00:18:27 +0900

    feat: 하나의 인스턴스로 user 상태관리를 하기 위해 싱글톤 패턴 적용(#124)

--- src/services/auth/authManager.js
+++ src/services/auth/authManager.js
@@ -1,8 +1,16 @@
 class AuthManager {
-	#userState;
+	static #instance; // class 변수
+	#userState; // 인스턴스 변수
 
 	constructor() {
+		// 하나의 인스턴스로 상태관리를 하기 위해서 싱글톤 패턴 적용
+		if (AuthManager.#instance) {
+			return AuthManager.#instance; // 이미 인스턴스가 존재하면 기존 인스턴스를 반환
+		}
+
+		// 인스턴스가 없으면 초기화하고 static 변수에 저장
 		this.#userState = this.#loadUserFromStorage();
+		AuthManager.#instance = this;
 	}
 
 	setState(newState) {
