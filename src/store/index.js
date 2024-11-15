2cf3bf151d9ff6c3621356d48fdf650d96fabcd3
gold <goldegg127@gmail.com>
2024-11-15 10:51:57 +0900

    feat: router 전역 상태관리자 생성 (#139)

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
-import authManager from './authManager';
+import { default as authManager } from './authManager';
+import { default as routerManager } from './routerManager';
 
-export { authManager };
+export { authManager, routerManager };
---
+++ src/store/routerManager.js
@@ -0,0 +1,44 @@
+class RouterManager {
+	static #instance;
+	#state;
+	#listeners = new Set();
+
+	constructor() {
+		if (RouterManager.#instance) {
+			return RouterManager.#instance;
+		}
+
+		this.#state = {
+			path: window.location.pathname,
+		};
+
+		RouterManager.#instance = this;
+	}
+
+	get path() {
+		return this.#state.path;
+	}
+
+	setState(newState) {
+		this.#state = { ...this.#state, ...newState };
+		this.#alarmListeners();
+	}
+
+	subscribeListener(listener) {
+		this.#listeners.add(listener);
+	}
+
+	unsubscribeListener(listener) {
+		this.#listeners.delete(listener);
+	}
+
+	#alarmListeners() {
+		this.#listeners.forEach((listener) => {
+			listener();
+		});
+	}
+}
+
+const routerManager = new RouterManager();
+
+export default routerManager;
